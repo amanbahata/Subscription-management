34060a6bb719a68f0c6d27b0c795cf6dd6bb2e1e
Aman Bahata <bahataaman@gmail.com>
2016-12-19 17:15:55 +0000

    Adds indentation to codebase

--- sp2/PhoneSubscription.java
+++ sp2/PhoneSubscription.java
@@ -25,9 +25,9 @@ public abstract class PhoneSubscription extends Subscription {
     /**
      * Constructs a new phone subscription according to parameters
      *
-     * @param subscriber the name of the subscriber; must not be null
-     * @param subscriptionName the name of the subscribed service; must not be null
-     * @param phoneNumber the phone number of the subscription
+     * @param subscriber            the name of the subscriber; must not be null
+     * @param subscriptionName      the name of the subscribed service; must not be null
+     * @param phoneNumber           the phone number of the subscription
      * @param standingChargeInPence the standing charge in pence; must not be negative
      */
     public PhoneSubscription(String subscriber, String subscriptionName, String phoneNumber, int standingChargeInPence) {
@@ -40,6 +40,7 @@ public abstract class PhoneSubscription extends Subscription {
 
     /**
      * Retrieve the phone number
+     *
      * @return the phone number of the phone subscription
      */
     public String getPhoneNumber() {
@@ -48,15 +49,19 @@ public abstract class PhoneSubscription extends Subscription {
 
     /**
      * It ads the duration of a call in minutes to the total calls duration
+     *
      * @param minutes duration of a single call
      */
     public void makeCall(int minutes) {
-        if (minutes <= 0){throw new IllegalArgumentException("Illegal negative or 0 argument for minutes.");}
+        if (minutes <= 0) {
+            throw new IllegalArgumentException("Illegal negative or 0 argument for minutes.");
+        }
         callMinutes += minutes;
     }
 
     /**
      * Retrieve the total of the calls duration in minutes
+     *
      * @return the total calls duration per billing period
      */
     public int getCallMinutes() {
@@ -72,6 +77,7 @@ public abstract class PhoneSubscription extends Subscription {
 
     /**
      * Sets the cost per minute of placing a call
+     *
      * @param charge cost per minute of a call
      */
     public void setCharge(int charge) {
@@ -81,6 +87,7 @@ public abstract class PhoneSubscription extends Subscription {
 
     /**
      * Retrieve the charge per minute of a call
+     *
      * @return charge per minute
      */
     public int getCharge() {
